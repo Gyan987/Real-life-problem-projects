8323c2d8bf17ed459ee729a09476fb50ea4a374f
Gyan <gyanprakasht987@gmail.com>
2026-03-27 14:29:04 +0530

    real world problem on arrays

---
+++ pro4.c
@@ -0,0 +1,48 @@
+// Problem Statement
+// You are given an array A of N positive integers. 
+
+// A contiguous subarray A[l...r] (where l < r, meaning the length is at least 2) is called Dominant if the maximum element within this subarray is strictly greater than the sum of all other elements in the same subarray. 
+
+// Mathematically, a subarray is Dominant if:
+// 2 * max(A[k]) > sum(A[k]) for all k from l to r.
+
+// Your task is to find the total number of Dominant subarrays in the given array A.
+
+// Solution:
+
+#include <stdio.h>
+
+int main() {
+
+    int N;
+    scanf("%d",&N);
+
+    long long A[N];
+    for(int i=0;i<N;i++){
+        scanf("%lld",&A[i]);
+    }
+
+    long long count=0;
+
+    for(int l=0;l<N;l++){
+        long long sum=A[l];
+        long long max=A[l];
+
+        for(int r=l+1;r<N;r++){
+
+            sum+=A[r];
+            if(A[r]>max) max=A[r];
+
+            if(2*max > sum){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+    printf("%lld",count);
+
+    return 0;
+}
